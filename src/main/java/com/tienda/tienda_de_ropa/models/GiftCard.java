4a11bf91b138849b6b57ea70fd476576772fcc1f
vghybrid <anderson.sarmiento13@gmail.com>
2022-11-23 16:19:24 -0300

    giftcard y gifcardDTO

--- src/main/java/com/tienda/tienda_de_ropa/models/GiftCard.java
+++ src/main/java/com/tienda/tienda_de_ropa/models/GiftCard.java
@@ -1,4 +1,89 @@
 package com.tienda.tienda_de_ropa.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
 public class GiftCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(strategy = "native", name = "native")
+    private long id;
+
+    private String numeroTarjeta;
+    private Double monto;
+    private int puntos;
+    private boolean activada = true;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    public GiftCard() {
+    }
+
+    public GiftCard(String numeroTarjeta, Double monto, int puntos, boolean activada, Cliente cliente) {
+        this.numeroTarjeta = numeroTarjeta;
+        this.monto = monto;
+        this.puntos = puntos;
+        this.activada = activada;
+        this.cliente = cliente;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public boolean isActivada() {
+        return activada;
+    }
+
+    public void setActivada(boolean activada) {
+        this.activada = activada;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftCard{" +
+                "id=" + id +
+                ", numeroTarjeta='" + numeroTarjeta + '\'' +
+                ", monto=" + monto +
+                ", puntos=" + puntos +
+                ", activada=" + activada +
+                ", cliente=" + cliente +
+                '}';
+    }
 }
