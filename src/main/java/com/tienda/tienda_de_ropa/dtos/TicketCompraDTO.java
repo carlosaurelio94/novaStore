c6803865a818971b2c00711f925c6568812bb2f7
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-23 19:20:32 -0300

    Ticket compras

---
+++ src/main/java/com/tienda/tienda_de_ropa/dtos/TicketCompraDTO.java
@@ -0,0 +1,46 @@
+package com.tienda.tienda_de_ropa.dtos;
+
+import com.tienda.tienda_de_ropa.models.TicketCompra;
+
+public class TicketCompraDTO {
+
+    private long id;
+
+    private String numeroTicket;
+
+    private String nombreProducto;
+
+    private int cantidadProducto;
+
+    private Double montoTotal;
+
+    public TicketCompraDTO() {}
+
+    public TicketCompraDTO(TicketCompra ticketCompra) {
+        this.id = ticketCompra.getId();
+        this.numeroTicket = ticketCompra.getNumeroTicket();
+        this.nombreProducto = ticketCompra.getNombreProducto();
+        this.cantidadProducto = ticketCompra.getCantidadProducto();
+        this.montoTotal = ticketCompra.getMontoTotal();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNumeroTicket() {
+        return numeroTicket;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public int getCantidadProducto() {
+        return cantidadProducto;
+    }
+
+    public Double getMontoTotal() {
+        return montoTotal;
+    }
+}
