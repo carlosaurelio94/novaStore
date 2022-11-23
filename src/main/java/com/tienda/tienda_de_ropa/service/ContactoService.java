c958f2a761c6f0709478a3082e7e2124a274e912
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-23 19:08:35 -0300

    Ticket compras

--- src/main/java/com/tienda/tienda_de_ropa/service/ContactoService.java
+++ src/main/java/com/tienda/tienda_de_ropa/service/ContactoService.java
@@ -1,7 +1,9 @@
 package com.tienda.tienda_de_ropa.service;
 
 import com.tienda.tienda_de_ropa.models.Contacto;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ContactoService {
     public void guardarContacto(Contacto contacto);
 
