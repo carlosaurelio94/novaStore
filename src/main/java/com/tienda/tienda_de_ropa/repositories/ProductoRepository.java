810d81f59f4d478a1303e603bb1a27c8a872e954
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-23 13:23:16 -0300

    anotaciones repositorios contacto y producto

--- src/main/java/com/tienda/tienda_de_ropa/repositories/ProductoRepository.java
+++ src/main/java/com/tienda/tienda_de_ropa/repositories/ProductoRepository.java
@@ -2,6 +2,8 @@ package com.tienda.tienda_de_ropa.repositories;
 
 import com.tienda.tienda_de_ropa.models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
