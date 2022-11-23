2bd9078551047d3a439c9e217982dbc246c79df6
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-23 13:13:48 -0300

    clases contacto y producto

--- src/main/java/com/tienda/tienda_de_ropa/models/Producto.java
+++ src/main/java/com/tienda/tienda_de_ropa/models/Producto.java
@@ -1,4 +1,86 @@
 package com.tienda.tienda_de_ropa.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native", strategy = "native")
+    private long id;
+
+    private String nombre;
+
+    private int stock;
+
+    private Double precio;
+
+    private String talle;
+
+
+
+    public Producto() {
+    }
+
+    public Producto(long id, String nombre, int stock, Double precio, String talle) {
+        this.id = id;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+        this.talle = talle;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getTalle() {
+        return talle;
+    }
+
+    public void setTalle(String talle) {
+        this.talle = talle;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", stock='" + stock + '\'' +
+                ", precio='" + precio + '\'' +
+                ", talle='" + talle + '\'' +
+                '}';
+    }
 }
