925039838fe54fc9b74fce7c23cd7f14ec71f4dc
vghybrid <anderson.sarmiento13@gmail.com>
2022-11-23 11:22:50 -0300

    esqueleto

---
+++ src/main/java/com/tienda/tienda_de_ropa/TiendaDeRopaApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.tienda_de_ropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDeRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDeRopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/tienda_de_ropa/TiendaDeRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.tienda_de_ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDeRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
