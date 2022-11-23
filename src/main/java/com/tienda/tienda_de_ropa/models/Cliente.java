eab33f1a132c499ba243ee9c353e894f6b7444eb
vghybrid <anderson.sarmiento13@gmail.com>
2022-11-23 12:35:50 -0300

    propiedades de cliente

--- src/main/java/com/tienda/tienda_de_ropa/models/Cliente.java
+++ src/main/java/com/tienda/tienda_de_ropa/models/Cliente.java
@@ -1,4 +1,135 @@
 package com.tienda.tienda_de_ropa.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator= "native")
+    @GenericGenerator(strategy= "native", name= "native")
+    private long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String clave;
+    private int puntos;
+
+    @OneToMany(mappedBy= "cliente", fetch= FetchType.EAGER)
+    private Set<Transaccion> transacciones = new HashSet<>();
+
+    @OneToMany(mappedBy= "cliente", fetch= FetchType.EAGER)
+    private Set<Contacto> contactos = new HashSet<>();
+
+    @OneToMany(mappedBy= "cliente", fetch= FetchType.EAGER)
+    private Set<ClienteGiftCard> clienteGiftCards = new HashSet<>();
+
+    @OneToMany(mappedBy= "cliente", fetch = FetchType.EAGER)
+    private Set<ClienteProducto> clienteProductos = new HashSet<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellido, String email, String clave, int puntos) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.clave = clave;
+        this.puntos = puntos;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public Set<Transaccion> getTransacciones() {
+        return transacciones;
+    }
+
+    public void setTransacciones(Set<Transaccion> transacciones) {
+        this.transacciones = transacciones;
+    }
+
+    public Set<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(Set<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+    public Set<ClienteGiftCard> getClienteGiftCards() {
+        return clienteGiftCards;
+    }
+
+    public void setClienteGiftCards(Set<ClienteGiftCard> clienteGiftCards) {
+        this.clienteGiftCards = clienteGiftCards;
+    }
+
+    public Set<ClienteProducto> getClienteProductos() {
+        return clienteProductos;
+    }
+
+    public void setClienteProductos(Set<ClienteProducto> clienteProductos) {
+        this.clienteProductos = clienteProductos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", clave='" + clave + '\'' +
+                ", puntos=" + puntos +
+                ", transacciones=" + transacciones +
+                ", contactos=" + contactos +
+                ", clienteGiftCards=" + clienteGiftCards +
+                ", clienteProductos=" + clienteProductos +
+                '}';
+    }
 }
--- src/main/java/com/tienda/tienda_de_ropa/models/ClienteProducto.java
+++ src/main/java/com/tienda/tienda_de_ropa/models/ClienteProducto.java
@@ -1,4 +1,76 @@
 package com.tienda.tienda_de_ropa.models;
 
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class ClienteProducto {
+    private long id;
+    private String nombreProducto;
+    private int cantidadProducto;
+
+    @ManyToOne(fetch= FetchType.EAGER)
+    @JoinColumn(name= "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne(fetch= FetchType.EAGER)
+    @JoinColumn(name= "producto_id")
+    private Producto producto;
+
+    public ClienteProducto() {
+    }
+
+    public ClienteProducto(String nombreProducto, int cantidadProducto, Cliente cliente, Producto producto) {
+        this.nombreProducto = nombreProducto;
+        this.cantidadProducto = cantidadProducto;
+        this.cliente = cliente;
+        this.producto = producto;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidadProducto() {
+        return cantidadProducto;
+    }
+
+    public void setCantidadProducto(int cantidadProducto) {
+        this.cantidadProducto = cantidadProducto;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    @Override
+    public String toString() {
+        return "ClienteProducto{" +
+                "id=" + id +
+                ", nombreProducto='" + nombreProducto + '\'' +
+                ", cantidadProducto=" + cantidadProducto +
+                ", cliente=" + cliente +
+                ", producto=" + producto +
+                '}';
+    }
 }
