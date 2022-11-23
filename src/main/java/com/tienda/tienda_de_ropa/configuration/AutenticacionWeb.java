7e2dc0af494963aabdba690b3b3b53c915246552
vghybrid <anderson.sarmiento13@gmail.com>
2022-11-23 18:07:24 -0300

    autorizacion y authenticacion

--- src/main/java/com/tienda/tienda_de_ropa/configuration/AutenticacionWeb.java
+++ src/main/java/com/tienda/tienda_de_ropa/configuration/AutenticacionWeb.java
@@ -4,6 +4,7 @@ import com.tienda.tienda_de_ropa.models.Cliente;
 import com.tienda.tienda_de_ropa.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -12,7 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@Configuration
 public class AutenticacionWeb extends GlobalAuthenticationConfigurerAdapter {
     @Autowired
     ClienteRepository clienteRepository;
@@ -34,7 +35,6 @@ public class AutenticacionWeb extends GlobalAuthenticationConfigurerAdapter {
             }
         });
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
