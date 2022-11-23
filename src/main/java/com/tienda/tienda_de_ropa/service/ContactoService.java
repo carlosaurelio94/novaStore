936faa2f3d6c22c9426230db2a5f50c8aca1959d
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-23 19:09:55 -0300

    Ticket compras

--- src/main/java/com/tienda/tienda_de_ropa/service/ContactoService.java
+++ src/main/java/com/tienda/tienda_de_ropa/service/ContactoService.java
@@ -3,7 +3,7 @@ package com.tienda.tienda_de_ropa.service;
 import com.tienda.tienda_de_ropa.models.Contacto;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface ContactoService {
     public void guardarContacto(Contacto contacto);
 
--- src/main/java/com/tienda/tienda_de_ropa/service/implementation/ContactoServiceImplement.java
+++ src/main/java/com/tienda/tienda_de_ropa/service/implementation/ContactoServiceImplement.java
@@ -2,13 +2,14 @@ package com.tienda.tienda_de_ropa.service.implementation;
 
 import com.tienda.tienda_de_ropa.models.Contacto;
 import com.tienda.tienda_de_ropa.repositories.ContactoRepository;
+import com.tienda.tienda_de_ropa.service.ContactoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
-public class ContactoServiceImplement {
+@Service
+public class ContactoServiceImplement implements ContactoService {
 
     @Autowired
     ContactoRepository contactoRepository;
