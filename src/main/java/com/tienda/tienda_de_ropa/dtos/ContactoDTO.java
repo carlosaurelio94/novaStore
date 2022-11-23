baac913a194ddf7d7cf10e33bf202b03cb84c831
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-23 13:19:57 -0300

    dto contacto y producto

--- src/main/java/com/tienda/tienda_de_ropa/dtos/ContactoDTO.java
+++ src/main/java/com/tienda/tienda_de_ropa/dtos/ContactoDTO.java
@@ -1,4 +1,32 @@
 package com.tienda.tienda_de_ropa.dtos;
 
+import com.tienda.tienda_de_ropa.models.Contacto;
+
 public class ContactoDTO {
+
+    private long id;
+
+    private String email;
+
+    private String apodo;
+
+    public ContactoDTO() {}
+
+    public ContactoDTO(Contacto contacto) {
+        this.id = contacto.getId();
+        this.email = contacto.getEmail();
+        this.apodo = contacto.getApodo();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
 }
