2bbc7f83f7144ad5d2801b3cce8192a846c540ec
ivommg <ivomoyanog@gmail.com>
2022-11-24 12:35:26 -0300

    creamos el dto

--- src/main/java/com/tienda/tienda_de_ropa/controllers/CompraController.java
+++ src/main/java/com/tienda/tienda_de_ropa/controllers/CompraController.java
@@ -1,17 +1,16 @@
 package com.tienda.tienda_de_ropa.controllers;
 
+import com.tienda.tienda_de_ropa.models.Compra;
 import com.tienda.tienda_de_ropa.models.TipoTransaccion;
 import com.tienda.tienda_de_ropa.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api")
@@ -30,6 +29,10 @@ public class CompraController {
         if (monto.isNaN() || monto == 0){
             return new ResponseEntity<>("No ingresaste un monto",HttpStatus.FORBIDDEN);
         }
+        if (){
+
+        }
+
 
         return new ResponseEntity<>("Se Realizo la transaccion con exito",HttpStatus.CREATED);
     }
