3eb8a4411b8ce6c5b4bc0c39fc6b3d8fbf0ef2a4
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-24 11:07:50 -0300

    espaciados

--- src/main/java/com/tienda/tienda_de_ropa/controllers/ContactoController.java
+++ src/main/java/com/tienda/tienda_de_ropa/controllers/ContactoController.java
@@ -55,7 +55,7 @@ public class ContactoController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/clients/current/contacts")
+    @GetMapping("/clientes/current/contactos")
     public Set<ContactoDTO> traerContactos(Authentication authentication) {
         return new ClienteDTO(clienteService.findByCorreo(authentication.getName())).getContactos();
     }
