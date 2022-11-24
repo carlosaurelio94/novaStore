d6d426409db040dc5f54bd9bc69016a37500b9fa
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-24 11:20:52 -0300

    arreglar espaciado contacto service

--- src/main/java/com/tienda/tienda_de_ropa/service/ContactoService.java
+++ src/main/java/com/tienda/tienda_de_ropa/service/ContactoService.java
@@ -1,7 +1,6 @@
 package com.tienda.tienda_de_ropa.service;
 
 import com.tienda.tienda_de_ropa.models.Contacto;
-import org.springframework.stereotype.Service;
 
 
 public interface ContactoService {
