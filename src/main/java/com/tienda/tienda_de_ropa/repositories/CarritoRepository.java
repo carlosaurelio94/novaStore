770710b77fbecb510dab965abeeaae03982558ed
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-25 16:57:34 -0300

    giftcard controller

--- src/main/java/com/tienda/tienda_de_ropa/repositories/CarritoRepository.java
+++ src/main/java/com/tienda/tienda_de_ropa/repositories/CarritoRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 =======
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
 @RepositoryRestResource
 >>>>>>> 8be4ddfe729bb561ae3b0d4afcbd93400b5279c8
 public interface CarritoRepository extends JpaRepository<Carrito, Long> {
