81e3b1d623242f599f8967aa67f2aa1e42701bba
Carlos Rodriguez <carlosarc10@gmail.com>
2022-11-30 14:23:02 -0300

    carrito

--- src/main/resources/static/web/assets/scripts/login.js
+++ src/main/resources/static/web/assets/scripts/login.js
@@ -1,8 +1,104 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: "Hola",
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      emailSignIn: "",
+      passwordSignIn: "",
+      passwordChange: ""
+    }
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+    iniciarSesion() {
+      let email = this.email.toLowerCase();
+      let password = this.password
+      return axios.post('/api/login', `correo=${email}&clave=${password}`)
+        .then(response => {
+          window.location.assign("./productos.html")
+        })
+        .catch(function (error) {
+          return Swal.fire({
+            icon: "error",
+            title: "Error " + error.response.status,
+            text: "Your email or password are incorrect. Try again",
+            confirmButtonColor: '#ff4545',
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+        })
+    },
+    registrarse() {
+      let email = this.email.toLowerCase()
+      let password = this.password
+      let firstName = this.firstName.toLowerCase()
+      let lastName = this.lastName.toLowerCase()
+      return axios.post('/api/clients', `firstName=${firstName}&lastName=${lastName}&email=${email}&password=${password}`)
+        .then(response => axios.post('/api/login', `email=${email}&password=${password}`))
+        .then(res => {
+          localStorage.setItem("cookie", "true")
+        })
+        .then(response => Swal.fire({
+          text: 'Succesful Registration',
+          title: 'Welcome to Nova Bank',
+          icon: 'success',
+          confirmButtonColor: '#24cb24',
+        }))
+        .then(response => window.location.assign("./accounts.html"))
+        .catch(function (error) {
+          return Swal.fire({
+            icon: "error",
+            title: "Error " + error.response.status,
+            text: error.response.data,
+            confirmButtonColor: '#ff4545',
+/*                 footer: '<a href="">Why do I have this issue?</a>'
+ */              })
+        })
+    },
+    changePassword() {
+      let password = this.passwordChange
+      return axios.patch('/api/clients/current', `password=${password}`).then(response => Swal.fire({
+        title: 'Password changed succesfully',
+        text: 'We have already deleted the previous one, this will now be your only password.',
+        icon: 'success',
+        confirmButtonColor: '#24cb24'
+      })).then(response => window.location.assign("./accounts.html"))
+        .catch(function (error) {
+          return Swal.fire({
+            icon: "error",
+            title: "Error " + error.response.status,
+            text: error.response.data,
+            confirmButtonColor: '#ff4545',
+/*                 footer: '<a href="">Why do I have this issue?</a>'
+*/              })
+        })
+    },
+    signUp() {
+      let container = document.getElementById('container');
+      return container.classList.add("right-panel-active")
+    },
+    signIn() {
+      let container = document.getElementById('container');
+      return container.classList.remove("right-panel-active")
+    },
+
+  },
+}).mount('#app')
+
+
+
 const inputs = document.querySelectorAll(".input-field");
 const toggle_btn = document.querySelectorAll(".toggle");
 const main = document.querySelector("main");
 const contenedor = document.querySelector('.contenedor_general')
-const colores =['#CB51EE','#0073BC','#38B75E','#DE365C']
+const colores = ['#CB51EE', '#0073BC', '#38B75E', '#DE365C']
 const bullets = document.querySelectorAll(".bullets span");
 const images = document.querySelectorAll(".image");
 
@@ -43,23 +139,23 @@ bullets.forEach((bullet) => {
 
 
 
-const figuras = () =>{
-  for(let i= 0; i <= 35; i++){
-      let figura = document.createElement('span')
-      let select = Math.round(colores.length * Math.random())     
+const figuras = () => {
+  for (let i = 0; i <= 35; i++) {
+    let figura = document.createElement('span')
+    let select = Math.round(colores.length * Math.random())
 
-      
 
-      figura.style.top = innerHeight*Math.random() + 'px'
-      figura.style.left = innerWidth*Math.random() + 'px'
-      figura.style.background = colores[select >= colores.length ?  select -1: select]
 
-      contenedor.appendChild(figura)
+    figura.style.top = innerHeight * Math.random() + 'px'
+    figura.style.left = innerWidth * Math.random() + 'px'
+    figura.style.background = colores[select >= colores.length ? select - 1 : select]
 
-      setInterval(() =>{
-          figura.style.top = innerHeight*Math.random() + 'px'
-          figura.style.left = innerWidth*Math.random() + 'px'
-      },2500)
+    contenedor.appendChild(figura)
+
+    setInterval(() => {
+      figura.style.top = innerHeight * Math.random() + 'px'
+      figura.style.left = innerWidth * Math.random() + 'px'
+    }, 2500)
   }
 }
 
