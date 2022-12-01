3c7bfa062d362fbba1dba32506403f2e159c4f4c
Sebastian <greenpark96@gmail.com>
2022-12-01 11:56:27 -0500

    Update responsive navbar

--- src/main/resources/static/web/assets/scripts/confirma-compra.js
+++ src/main/resources/static/web/assets/scripts/confirma-compra.js
@@ -248,3 +248,28 @@ const figuras = () =>{
 }
 
 figuras()
+
+
+
+const contenedor1 = document.querySelector('.contenedor_general1')
+
+
+const figuras1 = () =>{
+    for(let i= 0; i <= 35; i++){
+        let figura = document.createElement('span')
+        let select = Math.round(colores.length * Math.random())
+
+        figura.style.top = innerHeight*Math.random() + 'px'
+        figura.style.left = innerWidth*Math.random() + 'px'
+        figura.style.background = colores[select >= colores.length ?  select -1: select]
+
+        contenedor1.appendChild(figura)
+
+        setInterval(() =>{
+            figura.style.top = innerHeight*Math.random() + 'px'
+            figura.style.left = innerWidth*Math.random() + 'px'
+        },2500)
+    }
+}
+
+figuras1()
