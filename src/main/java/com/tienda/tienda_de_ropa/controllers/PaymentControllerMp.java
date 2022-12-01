9b733f4d8246d7ae532f674a04218f9ce086cc3e
vghybrid <anderson.sarmiento13@gmail.com>
2022-12-01 19:36:44 -0300

    mercadoPago ok!

--- src/main/java/com/tienda/tienda_de_ropa/controllers/PaymentControllerMp.java
+++ src/main/java/com/tienda/tienda_de_ropa/controllers/PaymentControllerMp.java
@@ -22,9 +22,9 @@ public class PaymentControllerMp {
     @Autowired
     PaymentServiceMP paymentServiceMP;
 
-    @PostMapping(value = "/paymentmp", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> generateUrl(@RequestBody String items) throws Exception {
-        List<ItemDTO> itemDTOList = PaymentUtils.stringToDTO(items);
-        return paymentServiceMP.getPaymentUrl(itemDTOList);
-    }
+//    @PostMapping(value = "/paymentmp", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Object> generateUrl(@RequestBody String items) throws Exception {
+//        List<ItemDTO> itemDTOList = PaymentUtils.stringToDTO(items);
+//        return paymentServiceMP.getPaymentUrl(itemDTOList);
+//    }
 }
